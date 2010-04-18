8e2f98970a414e2b81b26e6347b7527082eca574
chieredan <chieredan@af7a59cb-fe08-2c78-68fa-0137a8025037>
2010-04-18 11:05:23 +0000

    Poprawiłem problem który się pojawiał gdy użytkownik bez logowania się wchodził na podstrony (poprzez adres). Zacząłem dodawać Layouty, dla podstron. Zmodyfikowałem działanie strony podstawowej by poprostu redirectowala do innych stron (głupio wygląda w nagłówku Default.aspx)

--- trunk/Ustawienia.aspx.cs
+++ trunk/Ustawienia.aspx.cs
@@ -5,10 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Ustawienia : System.Web.UI.Page
+public partial class Ustawienia : RestrictedUserPage
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
 
-    }
 }
